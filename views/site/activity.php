557d1f4dfeec4a7edb04f82820ae67ee47ee7cb6
Augitaro <dimalab2018@gmail.com>
2024-05-27 13:31:10 +1000

    style fix №1

--- views/site/activity.php
+++ views/site/activity.php
@@ -40,6 +40,17 @@ $this->title = 'Чем займешься?';
                     </div>
                 </a>
             </li>
+            <li class="activity-list-item">
+                <a href="./academic.html">
+                    <h3>Еще какой нибудь вид занятий</h3>
+                    <div class="tag">Предметы</div>
+                    <div class="activity-link">
+                        <p>смотреть подробнее</p>
+                        <div class="arrow"></div>
+                    </div>
+                </a>
+            </li>
+
         </ul>
     </div>
 </section>
